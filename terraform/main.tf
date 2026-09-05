0474a51e77c4d1eff7e027f33b90bb5038a58c8a
naveenkumar291 <113194986+naveenkumar291@users.noreply.github.com>
2026-09-05 20:30:54 +0530

    Add DevSecOps security pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "app_logs" {
+  bucket = "devsecops-node-app-logs-demo"
+}
+
+resource "aws_security_group" "app_sg" {
+  name = "devsecops-app-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
